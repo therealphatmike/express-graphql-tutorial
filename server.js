107941e7020a319702c3f4983d8cc8bb77c9f142
Michael Smith <leogoesgrr@gmail.com>
2019-03-10 18:15:57 -0500

    added dice roll query

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import { buildSchema } from "graphql";
 const schema = buildSchema(`
     type Query {
         hello: String
+        rollDice(numDice: Int!, numSides: Int) : [Int]
     }
 `);
 
@@ -13,6 +14,13 @@ const schema = buildSchema(`
 const root = {
     hello: () => {
         return 'Hello, world!';
+    },
+    rollDice: ({numDice, numSides}) => {
+        let output = [];
+        for (let i = 0; i < numDice; i++) {
+            output.push(1 + Math.floor(Math.random() * numSides || 6));
+        }
+        return output;
     }
 };
 
