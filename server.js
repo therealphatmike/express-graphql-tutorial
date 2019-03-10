776861b7ebcbc4d63eeb715e6357247b3def4dfe
Michael Smith <leogoesgrr@gmail.com>
2019-03-10 13:24:31 -0500

    fixed es6 issue

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
-// import express from "express";
-// import graphqlHTTP from "express-graphql";
-// import buildSchema from "graphql";
-const express = require("express");
-const graphqlHTTP = require("express-graphql");
-const { buildSchema } = require("graphql");
+import express from "express";
+import graphqlHTTP from "express-graphql";
+import { buildSchema } from "graphql";
 
 // Construct schema using graphql schema lang
 const schema = buildSchema(`
